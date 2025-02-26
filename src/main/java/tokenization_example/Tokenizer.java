fe576f1288e5a19d900efc43193cc4d11e5edabe
Kyle Dewey <kyle.dewey@csun.edu>
2025-02-26 12:45:52 -0800

    code from class

--- src/main/java/tokenization_example/Tokenizer.java
+++ src/main/java/tokenization_example/Tokenizer.java
@@ -1,5 +1,7 @@
 package tokenization_example;
 
+import java.util.Optional;
+
 public class Tokenizer {
     public final String input;
     private int position;
@@ -9,10 +11,60 @@ public class Tokenizer {
         position = 0;
     }
 
-    private void skipWhitespace() {
+    public int getPosition() {
+        return position;
+    }
+    
+    public void skipWhitespace() {
         while (position < input.length() &&
                Character.isWhitespace(input.charAt(position))) {
             position++;
         }
-    }    
+    }
+
+    // private void readAnyToken() {
+    //     try {
+    //         return tryReadIntegerToken();
+    //     } catch (TokenizerException e) {
+    //         try {
+    //             return tryReadIdentifierToken();
+    //         } catch (TokenizerException e) {
+    //             try {
+    //                 return tryReadEqualsToken();
+    //             } catch (TokenizerException e) {
+    //                 ...
+
+    // returns null if it couldn't read a token
+    // private Token tryReadIntegerToken() {
+
+    public Optional<Token> tryReadIntegerToken() {
+        String digits = "";
+        while (position < input.length() &&
+               Character.isDigit(input.charAt(position))) {
+            digits += input.charAt(position);
+            position++;
+        }
+
+        if (digits.length() == 0) {
+            return Optional.empty();
+        } else {
+            return Optional.of(new IntegerToken(Integer.parseInt(digits)));
+        }
+    } // tryReadIntegerToken
+                                        
+    public Optional<Token> tryReadIdentifierToken() {
+        if (position < input.length() &&
+            Character.isLetter(input.charAt(position))) {
+            String chars = "" + input.charAt(position);
+            position++;
+            while (position < input.length() &&
+                   Character.isLetterOrDigit(input.charAt(position))) {
+                chars += input.charAt(position);
+                position++;
+            }
+            return Optional.of(new IdentifierToken(chars));
+        } else {
+            return Optional.empty();
+        }
+    }
 }
